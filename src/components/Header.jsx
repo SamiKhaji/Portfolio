29f57dba4d28988cd08e0fe258d7777c207bbb93
Mahammad Sami Khaji <kmahammadsami959@gmail.com>
2025-03-16 22:08:00 +0530

    project updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,11 +20,11 @@ export default function Header({ isNightMode, toggleNightMode }) {
         {/* Desktop Navigation */}
         <nav className="desktop-nav">
           <ul>
-            <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-            <li><Link to="education" smooth={true} duration={500}>Education & Experience</Link></li>
-            <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-            <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
-            <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+            <li className="effect"><Link to="home" smooth={true} duration={500}>Home</Link></li>
+            <li className="effect"><Link to="education" smooth={true} duration={500}>Education & Experience</Link></li>
+            <li className="effect"><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
+            <li className="effect"><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+            <li className="effect"><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
           </ul>
         </nav>
 
